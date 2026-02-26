19d023dc7797f5cfcc2db143ee69b2111e32f161
Midoweno <reguo@ucsd.edu>
2026-02-26 11:15:53 -0800

    Staging 2/26/26 edits

--- rankall.R
+++ rankall.R
@@ -1,4 +1,10 @@
+##LAPCDIR
+##setwd("C:/Users/guore/Downloads/rprog_data_ProgAssignment3-data")
+
+##mac dir
 ##setwd("/Users/renoguo/Documents/Coursera/rprog_data_ProgAssignment3-data")
+
+
 rankall <- function(outcome, num = "best") {
   
   
@@ -11,20 +17,44 @@ rankall <- function(outcome, num = "best") {
   ##pneumonia
   dfalloutcomes[, 23] <- as.numeric(dfalloutcomes[, 23])
   
+  
+  ##Lookup vector created (converts each string input to numbers to index columns)
   outcome_columns<-c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
+  colnum <- outcome_columns[outcome]
   
-  ## Checking that outcome is valid
-  if (outcome %in% c("heart attack","heart failure","pneumonia")){
-    #Checking that outcome is one of the 3 given outcomes, now continu
-    
-    
-    else{
+  
+  ## Checking that outcome is valid, if invalid, stop
+  if (!outcome %in% c("heart attack","heart failure","pneumonia")){
+    #Checking that outcome is one of the 3 given outcomes, now continue
       stop("invalid outcome")
-    }
   }
+  ##continue below
   
   
-  ## For each state, find the hospital of the given rank
-  ## Return a data frame with the hospital names and the
-  ## (abbreviated) state name
+  ##Ordering based on outcome
+  df <- dfalloutcomes[
+    order(dfalloutcomes[,7],
+          dfalloutcomes[,colnum],
+          dfalloutcomes[,2]),
+  ]
+  state_list<-split(df,df$State) ##split into states
+  rows_per_state<-sapply(state_list,nrow)
+  
+  
+  ##Now for "num" input, extract to a new df depending on input
+  if (num == "best"){
+      output<-do.call(rbind,lapply(state_list,function(x){x[1,]}))
+      output<-output[,c(2,7)]
+      output
+  } else if (num=="worst"){
+      output<-do.call(rbind,lapply(state_list,function(x){x[nrow(x),]}))
+      output<-output[,c(2,7)]
+      output
+  } else if(any(num>rows_per_state)) {
+    stop(NA)##### LEFT OFF HERE, CHANGE THIS SO NA REPLACES THE STATE WITH FEWER ROW THAN NUM INPUT
+  } else {
+    output<-do.call(rbind,lapply(state_list,function(x){x[num,]}))
+    output<-output[,c(2,7)]
+    output
+  }
 }
